751c8a2586152c7f674a8e73110fac48d4cd6d71
Apurwa <Bhattarai.Apurwa@gmail.com>
2023-03-29 02:16:28 -0500

    Initial Commit

---
+++ TikTakToe.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TikTakToe {
+    public static void main(String[] args) {
+
+        char[] input = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your choice");
+//        String value = scanner.next();
+        System.out.println( input[6] +      "  | " + input[7]            +    " | "  + input[8]);
+        System.out.println("---*---*-----");
+        System.out.println( input[3] +      "  | " + input[4]            +    " | "  + input[5]);
+        System.out.println("---*---*-----");
+        System.out.println( input[0] +      "  | " + input[1]            +    " | "  + input[2]);
+
+    }
+}
